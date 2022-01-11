7b1e24be3f71180aa0fc824994c486ecb385d85c
Momo Baz <64978845+bazforever@users.noreply.github.com>
2022-01-10 22:23:47 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,48 @@
+
+
+user = int(input(" Enter a number: "))
+count =0
+
+for i in range(user + 1):
+    count += i
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
